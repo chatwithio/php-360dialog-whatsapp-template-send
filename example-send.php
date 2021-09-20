df8c90963d0943fb59fe5b6867fe0e2e51d9ca5a
ward <wardazo@gmail.com>
2021-09-20 15:50:38 +0200

    Moving to main

--- example-send.php
+++ example-send.php
@@ -11,10 +11,10 @@ $o = new WhatsApp360();
 
 print_r(
     $o->sendWhatsApp(
-        "34622814642",
-        ["2"],
-        'sample_shipping_confirmation',
-        'en_US',
+        "654765476547654",
+        ["011111","Ward","$12.00","Macbook"],
+        'order_sent',
+        'en',
         'f6baa15e_fb52_4d4f_a5a0_cde307dc3a85'
     )
 );
